bae6a7507d9a064cc7d4e2c3a169766b07e31116
Sophia Clarke <sophiaclarke93@gmail.com>
2018-03-01 14:34:16 +0000

    All values (passed, failed, blocked) appear as a stacked bar

--- Assets/Scenes/Bar.cs
+++ Assets/Scenes/Bar.cs
@@ -9,8 +9,21 @@ public class Bar : MonoBehaviour {
 
 	public void CreateBar(ParseData.Result result)
 	{
-		GameObject currentSlider = Instantiate(sliderPrefab, this.transform, false);
-		currentSlider.GetComponent<Slider>().value = result.passed;
-		currentSlider.GetComponentInChildren<Image>().color = Color.green;
+		GameObject blockedSlider = Instantiate(sliderPrefab, this.transform, false);
+		blockedSlider.GetComponent<Slider>().value = result.passed + result.failed + result.blocked; //this one should render behind the failed slider, so only the failed amount should poke out
+		blockedSlider.GetComponentInChildren<Image>().color = Color.grey;
+		//Debug.Log("blocked " + blockedSlider.GetComponent<Slider>().value);
+
+		GameObject failedSlider = Instantiate(sliderPrefab, this.transform, false);
+		failedSlider.GetComponent<Slider>().value = result.passed + result.failed; //this one should render behind the passed slider, so only the failed amount should poke out
+		failedSlider.GetComponentInChildren<Image>().color = Color.red;
+		//Debug.Log("failed " + failedSlider.GetComponent<Slider>().value);
+
+		GameObject passedSlider = Instantiate(sliderPrefab, this.transform, false);
+		passedSlider.GetComponent<Slider>().value = result.passed;
+		passedSlider.GetComponentInChildren<Image>().color = Color.green;
+		//Debug.Log("passed " + passedSlider.GetComponent<Slider>().value);
+
+		
 	}
 }
